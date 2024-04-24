2eb70678f0c99dbaf692cbf2ce48950d6b8be89d
Иван Завадский <zavadskiy1998@yandex.ru>
2024-04-24 13:01:19 +0400

    DbContext

--- AtonTest/Models/AtDbContext.cs
+++ AtonTest/Models/AtDbContext.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
 
-
 namespace AtonTest.Models;
 
 public partial class AtDbContext : DbContext
@@ -19,25 +18,20 @@ public partial class AtDbContext : DbContext
         modelBuilder.Entity<User>(entity =>
         {
             entity.Property(p => p.Id).ValueGeneratedNever();
-            entity.Property(e => e.Birthday).HasColumnType("datetime");
+
+            entity.Property(p => p.CreatedBy).HasMaxLength(20).IsUnicode(false);
+
+            entity.Property(p => p.Birthday).HasColumnType("datetime");
+
+            entity.Property(p=>p.Login).HasMaxLength(20).IsUnicode(false);
+
+            entity.Property(p=>p.ModifiedBy).HasMaxLength(20).IsUnicode(false);
+
+            entity.Property(p=> p.Name).HasMaxLength(20).IsUnicode(false);
+
+            entity.Property(p=>p.Password).HasMaxLength(30).IsUnicode(false);
+
+            entity.Property(p=>p.RevorkedBy).HasMaxLength(20).IsUnicode(false);
         });
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
-        
     }
-
-
 }
